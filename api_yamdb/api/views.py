a8bdf1b34cf7a58f55ba4691805ef859e6c74892
Rustam Yunusov <rust.yunusov2015@yandex.ru>
2022-09-08 22:09:47 +0500

    добавлена вью функция для модели review, обновлен файл urls

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-from reviews.models import Comment, Review
+from reviews.models import Review, Titles
 from .serializers import CommentSerializer, ReviewSerializer
+# from .permissions import smth
 
 
-class ReviewSerializer(viewsets.ModelViewSet):
+class ReviewViewSet(viewsets.ModelViewSet):
     """Вьюсет для модели Review."""
 
     serializer_class = ReviewSerializer
+    # permission_classes = smth
+    pagination_class = PageNumberPagination
+
+    def get_title(self):
+        return get_object_or_404(Titles, id=self.kwargs.get('title_id'))
+
+    def get_queryset(self):
+        return self.get_title().reviews
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user, title=self.get_title()
+        )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     """Вьюсет для модели Comment."""
 
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly, )
+    # permission_classes = smth
+    pagination_class = PageNumberPagination
 
     def get_review(self):
         return get_object_or_404(Review, id=self.kwargs.get('review_id'))
