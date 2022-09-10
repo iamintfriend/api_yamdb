a1ea5e68621796d58b5adf5ff86ba7af307af352
Иван Чистяков <vanuko1679@yandex.ru>
2022-09-10 10:14:58 +0300

    Models done

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from .models import Category, Comment, Genre, GenreTitle, Review, Titles, User
+from .models import Category, Comment, Genre, Review, Titles
 
 
-admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Titles)
-admin.site.register(GenreTitle)
 admin.site.register(Review)
 admin.site.register(Comment)
