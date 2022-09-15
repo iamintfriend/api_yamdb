88cd167b67c83c04cb19ffa384624a5bd72f5497
Alex K <h_xx@mail.ru>
2022-09-16 00:01:36 +1100

    Вторая версия загрузчика данных в БД

--- api_yamdb/reviews/management/commands/create_database.py
+++ api_yamdb/reviews/management/commands/create_database.py
@@ -1,107 +1,106 @@
 import csv
 from django.core.management.base import BaseCommand
+import logging
 from reviews.models import Category, Title, Genre, GenreTitle, Review, Comment
 from users.models import User
 
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+
+files = [
+    {
+        'model': User,
+        'path': 'static/data/users.csv',
+        'fields': ['username', 'email', 'role'],
+    },
+    {
+        'model': Category,
+        'path': 'static/data/category.csv',
+        'fields': ['name', 'slug'],
+    },
+    {
+        'model': Genre,
+        'path': 'static/data/genre.csv',
+        'fields': ['name', 'slug'],
+    },
+    {
+        'model': Title,
+        'path': 'static/data/titles.csv',
+        'fields': ['name', 'year'],
+    },
+    {
+        'model': GenreTitle,
+        'path': 'static/data/genre_title.csv',
+        'fields': ['title_id', 'genre_id'],
+    },
+    {
+        'model': Review,
+        'path': 'static/data/review.csv',
+        'fields': ['title_id', 'text', 'author', 'pub_date', 'score'],
+    },
+    {
+        'model': Comment,
+        'path': 'static/data/comments.csv',
+        'fields': ['review_id', 'text', 'author', 'pub_date'],
+    },
+]
+
+
+def get_payload(row, fields):
+    """
+    Функция собирает словарь для создания новой записи в БД.
+
+    Args:
+        row: dict - Строка из csv в виде словаря.
+        fields: list - Список из полей в зависимости от модели.
+    Returns:
+        payload: dict - Словать с названиями полей и их значениями.
+    """
+    payload = dict()
+    for field in fields:
+        if field == 'author':
+            payload[field] = User.objects.get(pk=row['author'])
+        else:
+            payload[field] = row[field]
+
+    return payload
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open('static/data/users.csv') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.id = int(row['id'])
-                self.username = row['username']
-                self.email = row['email']
-                self.role = row['role']
-                User.objects.create(
-                    id=self.id,
-                    username=self.username,
-                    email=self.email,
-                    role=self.role
-                )
-
-        with open('static/data/category.csv') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.id = int(row['id'])
-                self.name = row['name']
-                self.slug = row['slug']
-                Category.objects.create(
-                    id=self.id,
-                    name=self.name,
-                    slug=self.slug,
-                )
-
-        with open('static/data/genre.csv') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.id = int(row['id'])
-                self.name = row['name']
-                self.slug = row['slug']
-                Genre.objects.create(
-                    id=self.id,
-                    name=self.name,
-                    slug=self.slug,
-                )
-
-        with open('static/data/titles.csv') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.id = int(row['id'])
-                self.name = row['name']
-                self.year = int(row['year'])
-                self.category = Category.objects.get(id=int(row['category']))
-                # self.genre = Genre.objects.get(id=int(row))
-                Title.objects.create(
-                    id=self.id,
-                    name=self.name,
-                    year=self.year,
-                    category=self.category
-                )
-
-        with open('static/data/genre_title.csv') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.id = int(row['id'])
-                self.title = Title.objects.get(id=int(row['title_id']))
-                self.genre = Genre.objects.get(id=int(row['genre_id']))
-
-                GenreTitle.objects.create(
-                    id=self.id,
-                    title=self.title,
-                    genre=self.genre,
-                )
-
-        with open('static/data/review.csv') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.id = int(row['id'])
-                self.title = Title.objects.get(id=int(row['title_id']))
-                self.text = row['text']
-                self.author = User.objects.get(id=int(row['author']))
-                self.pub_date = row['pub_date']
-                self.score = int(row['score'])
-                Review.objects.create(
-                    id=self.id,
-                    title=self.title,
-                    text=self.text,
-                    author=self.author,
-                    pub_date=self.pub_date,
-                    score=self.score
-                )
-
-        with open('static/data/comments.csv') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.id = int(row['id'])
-                self.review = Review.objects.get(id=int(row['review_id']))
-                self.text = row['text']
-                self.author = User.objects.get(id=int(row['author']))
-                self.pub_date = row['pub_date']
-                Comment.objects.create(
-                    id=self.id,
-                    review=self.review,
-                    text=self.text,
-                    author=self.author,
-                    pub_date=self.pub_date,
-                )
+        created = 0
+        skip = 0
+        err = 0
+        for file in files:
+            with open(file['path']) as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    id = row['id']
+                    model = file['model']
+                    if model.objects.filter(id=id).exists():
+                        logger.info(
+                            f'{model.__name__} id {id} уже существует'
+                            'и пропущен.'
+                        )
+                        skip += 1
+                        continue
+                    try:
+                        payload = get_payload(row, file['fields'])
+                        entry = model.objects.create(pk=id, **payload)
+                    except Exception as exc:
+                        logger.warning(
+                            f'Ошибка при создании записи {model.__name__} id'
+                            f'{id} - {type(exc).__name__}: {exc}. Перехожу к'
+                            'следующей записи.'
+                        )
+                        err += 1
+                    else:
+                        logger.info(f'Запись {entry} успешно создана.')
+                        created += 1
+        print(f'Создано: {created} Пропущено: {skip} Ошибок: {err}')
